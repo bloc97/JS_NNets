bd32939df74ffc54e393c2e35610d44ff965d894
bloc97 <blocbloc97@gmail.com>
2016-11-09 10:54:44 -0500

    Commit

---
+++ src/Demo.js
@@ -0,0 +1,45 @@
+var synaptic = require('synaptic'); // this line is not needed in the browser
+var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer,
+    Architect = synaptic.Architect;
+
+
+var demoNet = new Architect.Perceptron(2,3,1);
+
+var demoTrainer = new Trainer(demoNet);
+
+var trainXor = [
+  {
+    input: [0,0],
+    output: [0]
+  },
+  {
+    input: [0,1],
+    output: [1]
+  },
+  {
+    input: [1,0],
+    output: [1]
+  },
+  {
+    input: [1,1],
+    output: [0]
+  }
+];
+
+//console.log(demoTrainer.XOR());
+
+console.log(demoTrainer.train(trainXor, {
+    rate: .2,
+    //iterations: 20000,
+    error: .005//,
+    //shuffle: true,
+    //log: 1000//,
+    //cost: Trainer.cost.CROSS_ENTROPY
+}));
+
+for (var i=0, j=trainXor.length; i<j; i++) {
+    console.log(demoNet.activate(trainXor[i].input));
+};
